429b527d994e8808d22be27e66a029717519bd7f
Qassim Osseni <qassimosseni@gmail.com>
2024-07-22 14:44:21 +0200

    ajout de la barre de filtre

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include PgSearch::Model
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -11,4 +12,23 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
 
   has_one_attached :avatar
+
+  pg_search_scope :search_by_city, against: :address,
+  using: {
+    tsearch: { prefix: true }
+  }
+
+  pg_search_scope :search_by_pace, against: :running_pace,
+  using: {
+    tsearch: { prefix: true }
+  }
+  def city
+    address.split(',')[1].strip
+    # permet au filtre de prendre la ville de l'User qui est en 2e position dans la string adress !
+  end
+  scope :order_by_city_asc, -> { all.sort_by(&:city) }
+  # récupère toutes les villes, les tries par ordres croissants et les retourne
+  scope :order_by_city_desc, -> { all.sort_by(&:city).reverse }
+  # récupère toutes les villes, les tries par ordres decroissants et les retourne
+
 end
