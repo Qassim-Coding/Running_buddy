c120847461d9af7893f1384268a8b9401a0ec137
manuel loutoby <manuel.loutoby@gmail.com>
2024-07-22 15:00:24 +0200

    bonsweetalert

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
+pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.12.3/dist/sweetalert2.all.js"
