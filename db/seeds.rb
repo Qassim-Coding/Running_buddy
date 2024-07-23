1208a5b63555f75f9bb43e1f091f0e8bb1783bfc
alice emin <aliceemin@yahoo.com>
2024-07-23 13:17:00 +0200

    seeds-mardi

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 
 puts "Destroying all messages..."
 Message.destroy_all
@@ -35,7 +36,8 @@ user1 = User.create!(
   preference_hour: 6,
   relationship_number: 1,
   meeting_number: 2,
-
+  latitude: 48.5387219,
+  longitude: 2.6605287
 )
 file = File.open(user1.picture)
 user1.avatar.attach(io: file, filename: "nes.png", content_type: "image/png")
@@ -66,6 +68,8 @@ user2 = User.create!(
   preference_hour: 6,
   relationship_number: 2,
   meeting_number: 1,
+  latitude: 48.529797,
+  longitude: 2.654954
 )
 file = File.open(user2.picture)
 user2.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -96,6 +100,8 @@ user3 = User.create!(
   preference_hour: 6,
   relationship_number: 5,
   meeting_number: 4,
+  latitude: 48.540578,
+  longitude: 2.644153
 )
 file = File.open(user3.picture)
 user3.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -125,7 +131,9 @@ user4 = User.create!(
   preference_date: "Friday, Wednesday",
   preference_hour: 6,
   relationship_number: 2,
-  meeting_number: 3
+  meeting_number: 3,
+  latitude: 48.6618728,
+  longitude: 2.5695888
 )
 file = File.open(user4.picture)
 user4.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -155,7 +163,9 @@ user5 = User.create!(
   preference_date: "Monday, Friday",
   preference_hour: 6,
   relationship_number: 15,
-  meeting_number: 2
+  meeting_number: 2,
+  latitude: 48.531863,
+  longitude: 2.649412
 )
 file = File.open(user5.picture)
 user5.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -185,7 +195,9 @@ user6 = User.create!(
   preference_date: "Monday, Saturday",
   preference_hour: 8,
   relationship_number: 14,
-  meeting_number: 10
+  meeting_number: 10,
+  latitude: 48.5412579,
+  longitude: 2.6749157
 )
 file = File.open(user6.picture)
 user6.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -215,7 +227,9 @@ user7 = User.create!(
   preference_date: "Saturday, Sunday",
   preference_hour: 6,
   relationship_number: 11,
-  meeting_number: 21
+  meeting_number: 21,
+  latitude: 48.520947,
+  longitude: 2.677683
 )
 file = File.open(user7.picture)
 user7.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -245,7 +259,9 @@ user8 = User.create!(
   preference_date: "Wednesday, Sunday ",
   preference_hour: 6,
   relationship_number: 2,
-  meeting_number: 2
+  meeting_number: 2,
+  latitude: 48.543046627191,
+  longitude: 2.676467611037
 )
 file = File.open(user8.picture)
 user8.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -275,7 +291,9 @@ user9 = User.create!(
   preference_date: "Monday, Friday",
   preference_hour: 6,
   relationship_number: 10,
-  meeting_number: 150
+  meeting_number: 150,
+  latitude: 48.516182000058,
+  longitude: 2.626078008966
 )
 file = File.open(user9.picture)
 user9.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -305,7 +323,9 @@ user10 = User.create!(
   preference_date: "Monday, Wednesday",
   preference_hour: 6,
   relationship_number: 14,
-  meeting_number: 10
+  meeting_number: 10,
+  latitude: 48.509906008146,
+  longitude: 2.666121964653
 )
 file = File.open(user10.picture)
 user10.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
@@ -335,11 +355,105 @@ user11 = User.create!(
   preference_date: "Monday, Wednesday",
   preference_hour: 6,
   relationship_number: 2,
-  meeting_number: 2
+  meeting_number: 2,
+  latitude: 48.532445993522,
+  longitude: 2.664300996188
 )
 file = File.open(user11.picture)
 user11.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
 user11.save
+
+user12 = User.create!(
+  email: "roxanne.petit@gmail.com",
+  password: '123456',
+  password_confirmation: '123456',
+  reset_password_token: nil,
+  reset_password_sent_at: nil,
+  remember_created_at: nil,
+  created_at: Time.now,
+  updated_at: Time.now,
+  last_name: "Petit",
+  first_name: "Roxanne",
+  address: "45 avenue Thiers, Melun, France",
+  picture: "app/assets/images/roxannepetit.jpg",
+  description: "Experienced runner.",
+  phone_number: "+33 5 56 78 56 12",
+  running_pace: 6.5,
+  next_official_running_event: "Deauville Marathon",
+  preparation: "Marathon-specific training plans.",
+  how_long_have_you_been_running: "3 years",
+  run_frequency: 7,
+  other_sport: "Cycling",
+  preference_date: "Monday, Thursday",
+  preference_hour: 5,
+  relationship_number: 8,
+  meeting_number: 3,
+  latitude: 48.529626991846,
+  longitude: 2.654874037325
+)
+file = File.open(user12.picture)
+user12.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
+user12.save
+
+pictures12 = [
+  'https://dgtzuqphqg23d.cloudfront.net/uCJw2yiUZDGzpyHSjHq4sqRJKSuXtCY55Nbr4ibEa4g-1536x2048.jpg',
+  'https://dgtzuqphqg23d.cloudfront.net/XoNievkKs0OpteH4ng0P5_6vwGx3tFMPZl245zpVyUM-1536x2048.jpg',
+  'https://dgtzuqphqg23d.cloudfront.net/HxJE5uDFwqtmGejRUE9aq7Zjl8bfehWsb259idXJmF4-1536x2048.jpg',
+  'https://dgtzuqphqg23d.cloudfront.net/IV6Di3mi7z2Dku6dE-qEdPUPIhclMW6JjQyWYlV3-Fk-1536x2048.jpg',
+  'https://dgtzuqphqg23d.cloudfront.net/KZcorMSK9pWg3-8OVEq52O1JXxydEPxm5JaL0w1obfY-1536x2048.jpg'
+]
+
+pictures12.each do |picture|
+  file = URI.open(picture)
+  user12.photos.attach(io: file, filename: 'activity', content_type: 'image/jpg')
+  user12.save
+end
+
+user13 = User.create!(
+  email: "manuel.loutoby@gmail.com",
+  password: '123456',
+  password_confirmation: '123456',
+  reset_password_token: nil,
+  reset_password_sent_at: nil,
+  remember_created_at: nil,
+  created_at: Time.now,
+  updated_at: Time.now,
+  last_name: "Loutoby",
+  first_name: "Manuel",
+  address: "57 avenue Thiers, Melun, France",
+  picture: "app/assets/images/manuelloutoby.jpg",
+  description: "Passionate runner.",
+  phone_number: "+33 7 56 78 56 12",
+  running_pace: 6.8,
+  next_official_running_event: "Deauville Marathon",
+  preparation: "Marathon-specific training plans.",
+  how_long_have_you_been_running: "2 years",
+  run_frequency: 10,
+  other_sport: "Boxing",
+  preference_date: "Monday, Thursday",
+  preference_hour: 9,
+  relationship_number: 4,
+  meeting_number: 3,
+  latitude: 48.529177062498,
+  longitude: 2.654663652684
+)
+file = File.open(user13.picture)
+user13.avatar.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
+user13.save
+
+pictures13 = [
+  'https://dgtzuqphqg23d.cloudfront.net/J4an4cYTt_pGqgfBJxneJkUJ0rQpQmOw2WUDqc8UVuM-1536x2048.jpg',
+  'https://dgtzuqphqg23d.cloudfront.net/O4D6S2JS2qSlrRLyTC5IOH27ttmpGqtTfa6AuEs0Rug-1536x2048.jpg',
+  'https://dgtzuqphqg23d.cloudfront.net/E0u93A_-ENgPKLEB7gVwa-38HF-eR4iSsRPl_13FuGQ-2048x1367.jpg',
+  'https://dgtzuqphqg23d.cloudfront.net/cJaY5c4jC5YiovoDUOQqdkaVT2uAsHEivgLLJOuQ5aM-2048x1367.jpg',
+  'https://dgtzuqphqg23d.cloudfront.net/sDEWuBwJ9iGUX6b5R8-JydmHZ39vHoWUWivRKufzEhk-1365x2048.jpg'
+]
+
+pictures13.each do |picture|
+  file = URI.open(picture)
+  user13.photos.attach(io: file, filename: 'activity', content_type: 'image/jpg')
+  user13.save
+end
 
 puts "end of creating users"
 Meeting.create!(
